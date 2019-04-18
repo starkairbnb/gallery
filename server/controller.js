103718e064a8fbc513c5abd4cb50d31b205db66b
Liezel Manalo <liezelanne9@gmail.com>
2019-04-18 16:15:06 -0700

    Extend the existing API to support all CRUD operations

--- server/controller.js
+++ server/controller.js
@@ -8,6 +8,31 @@ const controller = {
       })
       .then(data => res.status(200).send(data))
       .catch(err => res.status(404).send(err))
+  },
+  getAll: (req, res) => {
+    db
+      .find({})
+      .then(data => res.status(200).send(data))
+      .catch(err => res.status(404).send(err))
+  },
+  createOne: (req, res) => {
+    let { id, urls, title, location } = req.body;
+    db
+    .create({ id, urls, title, location })
+    .then(data => res.status(200).send('success'))
+    .catch(err => res.status(404).send(err))
+  },
+  delete: (req, res) => {
+    db
+    .deleteOne({ id: req.params.id })
+    .then(data => res.status(200).send('success'))
+    .catch(err => res.status(404).send(err))
+  },
+  update: (req, res) => {
+    db
+    .updateOne({ id: req.params.id }, req.body)
+    .then(data => res.status(200).send('success'))
+    .catch(err => res.status(404).send(err))
   }
 }
 
--- server/router.js
+++ server/router.js
@@ -5,5 +5,13 @@ const router = express.Router();
 router
   .route('/photos/:id')
   .get(controller.get)
+  .delete(controller.delete)
+  .put(controller.update)
+
+router
+  .route('/photos/')
+  .get(controller.getAll)
+  .post(controller.createOne)
+  
 
 module.exports = router;
