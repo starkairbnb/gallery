85222e151bfdfe5b5ebc51b6a62c232144b2fd47
Liezel Manalo <liezelanne9@gmail.com>
2019-04-25 09:10:27 -0700

    Modified controller to Sequelize

--- server/controller.js
+++ server/controller.js
@@ -1,42 +1,34 @@
-const db = require('../database/index.js');
+const db = require('../database/postgresModel');
 
 const controller = {
   get: (req, res) => {
-    let { id } = req.params;
-    console.log('recieved request for id: ', id)
     db
-      .find({ id })
-      .then(data => {
-        console.log(data)
-        res.status(200).send(data)
-      })
+      .findAll({ where: { id: req.params.id } })
+      .then(data => res.status(200).send(data))
       .catch(err => res.status(404).send(err))
   },
   getAll: (req, res) => {
     db
-      .find({})
+      .findAll({})
       .then(data => res.status(200).send(data))
       .catch(err => res.status(404).send(err))
   },
   createOne: (req, res) => {
-    console.log(req.body)
-    let { id, prop_id, url } = req.body;
+    let { id, urls, title, location } = req.body;
     db
-    .create({ id, prop_id, url })
+    .create({ id, urls, title, location })
     .then(data => res.status(200).send('success'))
     .catch(err => res.status(404).send(err))
   },
   delete: (req, res) => {
-    let { id } = req.params;
     db
-    .deleteOne({ id })
+    .destroy({ where: { id: req.params.id } })
     .then(data => res.status(200).send('success'))
     .catch(err => res.status(404).send(err))
   },
   update: (req, res) => {
-    let { id } = req.params;
     db
-    .updateOne({ id }, req.body)
+    .update(req.body, { where: { id: req.params.id } })
     .then(data => res.status(200).send('success'))
     .catch(err => res.status(404).send(err))
   }
